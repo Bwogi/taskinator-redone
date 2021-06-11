8acf4a954ff68a6a0ecf7c0613bd1df5c3e9cda3
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-06-11 07:56:57 -0700

    createTaskHandler function works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
-var buttonEl = document.querySelector('#save-task');
+// var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector('#task-form');
 var listEl = document.querySelector('#tasks-to-do');
-var nameInput = document.querySelector('#task-name');
-var selectInput = document.querySelector('#task-type')
+// var nameInput = document.querySelector('#task-name');
+var nameInput = document.querySelector("input[name='task-name']");
+// var selectInput = document.querySelector('#task-type')
+var selectInput = document.querySelector("select[name='task-type']");
+console.log(selectInput);
 
-buttonEl.addEventListener('click', function(event) {
+// formEl.addEventListener("submit", myFunction);
+
+// buttonEl.addEventListener('click', function(event) 
+
+const createTaskHandler = (event) => {
     // don't reload or refresh page 
     event.preventDefault();
     // create a list item element 
@@ -11,13 +19,19 @@ buttonEl.addEventListener('click', function(event) {
     // give it a class name 
     listItemEl.className = 'task-item';
     // capture value from form input as list item text content
-    listItemEl.innerHTML = nameInput.value;
-    listItemEl.innerHTML += "<br>" + selectInput.value;
+    // listItemEl.innerHTML = nameInput.value;
+    // listItemEl.innerHTML += "<br>" + selectInput.value;
+    listItemEl.textContent = nameInput.value;
+    listItemEl.textContent += " " + selectInput.value;
+
 
     // place it back onto the DOM on the html virtual page
     listEl.appendChild(listItemEl);
 
-})
+}
+
+formEl.addEventListener("submit", createTaskHandler);
+
 
 // console.log(buttonEl)
 
@@ -31,6 +45,7 @@ buttonEl.addEventListener('click', function(event) {
 //     // which element type do you want to create? li 
 //     // does it have a class? if yes, what is it?  task-item
 //     // ---------------------
+
 
 
 //     // lets capture the area for list items 
