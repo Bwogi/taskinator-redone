dafbaf1e0ff4c54ffe9bf91cf47b2202b2aa9614
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-06-13 09:36:52 -0700

    form handler done

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,41 +22,16 @@ const createTaskHandler = (event) => {
     // listItemEl.innerHTML = nameInput.value;
     // listItemEl.innerHTML += "<br>" + selectInput.value;
     listItemEl.textContent = nameInput.value;
-    listItemEl.textContent += " " + selectInput.value;
+    // listItemEl.textContent += " " + selectInput.value;
 
-
+    // lets create a div to hold the task client Information
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // lets add html content to it 
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + nameInput.value + "</h3>" + "<span class=''>" + selectInput.value + "</span>";
     // place it back onto the DOM on the html virtual page
-    listEl.appendChild(listItemEl);
+    listEl.appendChild(taskInfoEl);
 
 }
 
 formEl.addEventListener("submit", createTaskHandler);
-
-
-// console.log(buttonEl)
-
-// Ask TA about refactoring this code as in
-// var createTaskHandler = function(){}
-
-
-
-// buttonEl.addEventListener("click", function() {
-//     // qns: where in the html code do you want to place the list item? ul.task-list
-//     // which element type do you want to create? li 
-//     // does it have a class? if yes, what is it?  task-item
-//     // ---------------------
-
-
-
-//     // lets capture the area for list items 
-//     var taskListEl = document.querySelector(".task-list");
-//     // then we create a list item 
-//     var taskItemEl = document.createElement("li");
-//     // then we add a the same class as the previous list item 
-//     taskItemEl.className = "task-item";
-//     // add text content to the list item 
-//     taskItemEl.textContent = "new task appended";
-//     // we add the list item to the list items area 
-//     taskListEl.appendChild(taskItemEl);
-
-// });
