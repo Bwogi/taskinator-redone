9fea4551aba9610fa8b15ce83a8d38f9648db639
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-06-13 14:06:32 -0700

    list appendage eroor corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 // var buttonEl = document.querySelector('#save-task');
 var formEl = document.querySelector('#task-form');
 var listEl = document.querySelector('#tasks-to-do');
@@ -37,14 +38,22 @@ const createTaskEl = (theObject) => {
     var listItemEl = document.createElement('li');
     // give it a class name 
     listItemEl.className = 'task-item';
+    // lets add a task id 
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
     // lets create a div to hold the task client Information
     var taskInfoEl = document.createElement("div");
     // give it a class 
     taskInfoEl.className = "task-info";
     // lets add html content to it 
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + theObject.name + "</h3><span class='task-type'>" + theObject.type + "</span>";
+
+    // add these to the list item 
+    listItemEl.appendChild(taskInfoEl);
     // place it back onto the DOM on the html virtual page
-    listEl.appendChild(taskInfoEl);
+    listEl.appendChild(listItemEl);
+
+    // increase task counter for next unique id 
+    taskIdCounter++;
 };
 
 formEl.addEventListener("submit", taskFormHandler);
